b827f7ab96eebcd1f42d74f36426f99a54f528b6
Aliaksandr <djvalentinoff@gmail.com>
2023-12-06 15:01:24 +0300

    feat: add validation and mask phone

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
+import checkTextInputs from './modules/checkTextInputs';
 import forms from './modules/forms';
+import mask from './modules/mask';
 import modals from "./modules/modals";
 import sliders from './modules/sliders';
 
@@ -9,4 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
   sliders(".main-slider-item", "vertical");
   sliders(".feedback-slider-item", "horizontal", ".main-prev-btn", ".main-next-btn");
   forms();
+  mask("[name='phone']");
+  checkTextInputs("[name='name']");
+  checkTextInputs("[name='message']");
 })
