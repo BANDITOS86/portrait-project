e8e5b4fd221282ca0ce1f2ca98d18ec2a160b17c
Aliaksandr <djvalentinoff@gmail.com>
2023-12-09 11:59:28 +0300

    feat: add logic filter

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import modals from "./modules/modals";
 import showMoreStyles from './modules/showMoreStyles';
 import sliders from './modules/sliders';
 import calc from './modules/calc';
+import filter from './modules/filter';
 
 window.addEventListener("DOMContentLoaded", () => {
   "use_strict";
@@ -18,4 +19,5 @@ window.addEventListener("DOMContentLoaded", () => {
   checkTextInputs("[name='message']");
   showMoreStyles(".button-styles", "#styles .row");
   calc("#size", "#material", "#options", ".promocode", ".calc-price");
+  filter();
 })
